8bfa2af87c64f0a77c4d9f8319b8449ccd7b6e19
mytriagoco@gmail.com <mytriagoco@gmail.com>
2021-09-22 11:48:38 -0400

    frontend

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/icon.png">
+    <SignIn msg="Welcome to Your Grupomania Network"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SignIn from '@/components/SignIn.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    SignIn
+  }
+}
+</script>
