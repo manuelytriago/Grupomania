44743d5ed008ee62ba30323b78a7115738f6d248
mytriagoco@gmail.com <mytriagoco@gmail.com>
2022-03-03 10:25:52 -0500

    Sign in check

--- frontend/src/components/LogIn.vue
+++ frontend/src/components/LogIn.vue
@@ -5,9 +5,7 @@
       <img class="mb-4 img-fluid" src="../assets/images/icon-left-font-monochrome-white.svg" alt="Grupomania">
         
       <form>
-        
         <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
         <div class="form-floating">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="username" required>
           <label for="floatingInput">Email address</label>
@@ -60,19 +58,24 @@ export default {
         this.$router.push('/signup')
       },
       SignInUser(event) {
-      event.preventDefault()    
+      event.preventDefault() 
+      let answer = document.getElementById("response"); 
+      if (this.username != "" && this.password != "") {
+         
         let url = "http://localhost:3000/api/auth/login"
         let data1 = {
           email : this.username,
           password : this.password,
         }
-        let answer = document.getElementById("response"); 
         this.$http.post(url,data1).then(response => {
           this.$store.commit('login',response); 
           this.$router.push('/dashboard')
           }).catch(error => {
              answer.innerHTML =  error.response.data.message;
           });
+      }else{
+             answer.innerHTML =  "Please fill the fields in order to sign in";
+      }
     
     },
     
